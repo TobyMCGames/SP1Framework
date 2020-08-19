654e2a94b4184ec927ec77c062a8ac32643aeddf
rimaz08 <69342040+rimaz08@users.noreply.github.com>
2020-08-19 11:01:48 +0800

    state temp

--- SP1Framework/UI.cpp
+++ SP1Framework/UI.cpp
@@ -1,8 +1,18 @@
 #include "UI.h"
 
 UI::UI() :
-	life(5)
+	life(5),
+	x(9),
+	y(27),
+	state{ }
 {
+	for (int row = 0; row < x; row++)
+	{
+		for (int col = 0; col < y; col++)
+		{
+			state[row][col] = ' ';
+		}
+	}
 }
 
 UI::~UI()
@@ -17,4 +27,114 @@ void UI::renderlife(Console& anotherC)
 	c.X = 0;
 	c.Y = 0;
 	anotherC.writeToBuffer(c, ss.str());
+}
+
+void UI::loadstate()
+{
+	if (life > 3)
+	{
+		std::ifstream f;
+		f.open("");
+		std::string data;
+		int row = 0;
+		int col = 0;
+		while (getline(f, data))
+		{
+			for (int datarow = 0; datarow < (x * 2 - 1); datarow++) {
+				if (data[datarow] == ',')
+				{
+					continue;
+				}
+				else
+				{
+					state[row][col] = data[datarow];
+					row++;
+				}
+
+			}
+			row = 0;
+			col++;
+		}
+		f.close();
+	}
+	if (life < 2)
+	{
+		std::ifstream f;
+		f.open("");
+		std::string data;
+		int row = 0;
+		int col = 0;
+		while (getline(f, data))
+		{
+			for (int datarow = 0; datarow < (x * 2 - 1); datarow++) {
+				if (data[datarow] == ',')
+				{
+					continue;
+				}
+				else
+				{
+					state[row][col] = data[datarow];
+					row++;
+				}
+
+			}
+			row = 0;
+			col++;
+		}
+		f.close();
+	}
+	else
+	{
+		std::ifstream f;
+		f.open("");
+		std::string data;
+		int row = 0;
+		int col = 0;
+		while (getline(f, data))
+		{
+			for (int datarow = 0; datarow < (x * 2 - 1); datarow++) {
+				if (data[datarow] == ',')
+				{
+					continue;
+				}
+				else
+				{
+					state[row][col] = data[datarow];
+					row++;
+				}
+
+			}
+			row = 0;
+			col++;
+		}
+		f.close();
+	}
+}
+	
+	
+
+void UI::renderstate(Console& anotherC)
+{
+	for (int row = 0; row < x; row++)
+	{
+		for (int col = 0; col < y; col++)
+		{
+			if (state[row][col] == 'B') //Border for us to see
+			{
+				anotherC.writeToBuffer(row, 4 + col, "Û", 0x9B);
+			}
+			else if ((state[row][col] == 'G'))
+			{
+				anotherC.writeToBuffer(row, 4 + col, "Û", 0x7E);
+			}
+			else if ((state[row][col] == 'Y'))
+			{
+				anotherC.writeToBuffer(row, 4 + col, "Û", 0x7E);
+			}
+			else
+			{
+				anotherC.writeToBuffer(row, 4 + col, " ", 0x7F);
+			}
+		}
+	}
 }
--- SP1Framework/UI.h
+++ SP1Framework/UI.h
@@ -2,15 +2,21 @@
 #include <sstream>
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "Framework\console.h"
 class UI
 {
 private:
 	int life; //placeholder
+	int x;
+	int y;
+	char state[9][27];
 public:
 	UI();
 	~UI();
 
 	void renderlife(Console& anotherC);
+	void loadstate();
+	void renderstate(Console& anotherC);
 };
 
