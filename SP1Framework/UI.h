b4c2a996bbbdaed9d25c0bacb956f3c81ada7707
rimaz08 <69342040+rimaz08@users.noreply.github.com>
2020-08-21 10:30:49 +0800

    stat fix

--- SP1Framework/UI.h
+++ SP1Framework/UI.h
@@ -8,7 +8,7 @@
 class UI
 {
 private:
-	char state[9][27];
+	char state[33][33];
 	char dgrid[13][23];
 	char dicons[5][10];
 	Player User;
@@ -23,13 +23,5 @@ public:
 	void renderstate(Console& anotherC);
 	void loadgrid();
 	void rendergrid(Console& anotherC);
-	void loadvolcanoicon();
-	void rendervolcanoicon(Console& anotherC);
-	void loadearthquakeicon();
-	void renderearthquakeicon(Console& anotherC);
-	void loadtsunamiicon();
-	void rendertsunamiicon(Console& anotherC);
-	void loadtornadoicon();
-	void rendertornadoicon(Console& anotherC);
 };
 
